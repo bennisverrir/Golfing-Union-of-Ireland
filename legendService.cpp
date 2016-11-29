94076000cf9242c06530e8dd231d2a7f192d197f
Þorfinnur Pétursson <Thorfinnur@orfinnurs-MacBook-Pro.local>
2016-11-29 16:49:47 +0000

    setja sort inn

--- legendService.cpp
+++ legendService.cpp
@@ -1,6 +1,38 @@
+/*
 #include "legendService.h"
-#include "dataaccess.h"
+#include <algorithm>
 
+struct legendComparison
+{
+    bool operator() (Legend i,Legend j){return(i.getName()<j.getName());}
+};
+LegendService::LegendService()
+{
+
+}
+
+vector <Legend> LegendService::getLegends(){
+    vector <Legend> legend;
+    legend = a.readFile();
+
+    legendComparison cmp;
+    sort(legend.begin(), legend.end(), cmp);
+    return legend;
+}
+void addLegend(string name, char gender, int born, int death){
+    Legend person (name, gender, born, death);
+
+    a.writeFile(person);
+}
+*/
+#include "legendService.h"
+#include <iostream>
+#include <algorithm>
+
+struct legendComparison
+{
+    bool operator() (Legend i,Legend j){return(i.getName()<j.getName());}
+};
 
 LegendService::LegendService()
 {
@@ -9,12 +41,12 @@ LegendService::LegendService()
 
 vector <Legend> LegendService::getLegends(){
     vector <Legend> legend;
-    dataAccess a;
     legend = a.readFile();
+    legendComparison cmp;
+    sort(legend.begin(), legend.end(), cmp);
     return legend;
 }
-void addLegend(string name, char gender, int born, int death){
-    Legend a (name, gender, born, death);
-    dataAccess braeikka;
-    braeikka.writeFile(a);
+void LegendService::addLegend(string name, char gender, int born, int death){
+    Legend person (name, gender, born, death);
+    a.writeFile(person);
 }
--- legendService.h
+++ legendService.h
@@ -1,17 +1,20 @@
 #ifndef LEGENDSERVICE_H
 #define LEGENDSERVICE_H
-
+#include "dataaccess.h"
 #include <vector>
 #include "legend.h"
+
 
 using namespace std;
 
 class LegendService
 {
+    dataAccess a;
 public:
     LegendService();
     vector <Legend> getLegends ();
-    addLegend ();
+
+   void addLegend (string name, char gender, int born, int death);
 };
 
 #endif // LEGENDSERVICE_H
