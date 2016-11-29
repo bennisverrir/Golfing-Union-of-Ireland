c3d4a3539a7e7481f5c8b57b6ecf5689f57d6316
saraf16 <saraf16@ru.is>
2016-11-29 16:53:25 +0000

    Update legendService.cpp

--- legendService.cpp
+++ legendService.cpp
@@ -1,30 +1,3 @@
-/*
-#include "legendService.h"
-#include <algorithm>
-
-struct legendComparison
-{
-    bool operator() (Legend i,Legend j){return(i.getName()<j.getName());}
-};
-LegendService::LegendService()
-{
-
-}
-
-vector <Legend> LegendService::getLegends(){
-    vector <Legend> legend;
-    legend = a.readFile();
-
-    legendComparison cmp;
-    sort(legend.begin(), legend.end(), cmp);
-    return legend;
-}
-void addLegend(string name, char gender, int born, int death){
-    Legend person (name, gender, born, death);
-
-    a.writeFile(person);
-}
-*/
 #include "legendService.h"
 #include <iostream>
 #include <algorithm>
