c23138c54926367e6280b851a057f6a8cb452238
kolliarnorsson <kolbeinna16@ru.is>
2016-11-29 14:10:26 +0000

    Breytti dataaccess skranum

--- dataaccess.cpp
+++ dataaccess.cpp
@@ -4,3 +4,61 @@ dataAccess::dataAccess()
 {
 
 }
+
+vector<Legend> dataAccess::readFile()
+{
+    ifstream file;
+
+    vector<Legend> returnVector;
+
+    file.open("legends.txt");
+    string line;
+    string name;
+    char gender;
+    int born;
+    int death;
+
+    if(file.is_open())
+    {
+        while(getline(file,line))
+        {
+            stringstream linestream(line);
+
+            string sBorn;
+            string sDeath;
+            string sGender;
+
+            getline(linestream, name, ',');
+            getline(linestream, sGender, ',');
+            getline(linestream, sBorn, ',');
+            getline(linestream, sDeath, ',');
+
+            born = atoi(sBorn.c_str());
+            death = atoi(sDeath.c_str());
+            gender = sGender[0];
+
+            Legend tempLegend(name, gender, born, death);
+
+            returnVector.push_back(tempLegend);
+
+        }
+    }
+
+    file.close();
+
+    return returnVector;
+}
+void dataAccess::writeFile(Legend writeLegend)
+{
+    ofstream file;
+
+    file.open("legends.txt", ios::app);
+
+    if(file.is_open())
+    {
+        file << endl << writeLegend.getName() << "," << writeLegend.getGender() << ","
+             << writeLegend.getBorn << "," << writeLegend.getDeath;
+    }
+
+    file.close();
+}
--- dataaccess.h
+++ dataaccess.h
@@ -1,11 +1,22 @@
 #ifndef DATAACCESS_H
 #define DATAACCESS_H
 
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include "legend.h"
+
+using namespace std;
 
 class dataAccess
 {
 public:
     dataAccess();
+
+    vector<Legend> readFile();
+    void writeFile(Legend writeLegend);
 };
 
 #endif // DATAACCESS_H
