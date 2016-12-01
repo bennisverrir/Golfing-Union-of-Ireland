5a071d3a9fb62feec58ce822233cc1b7bee36746
kolliarnorsson <kolbeinna16@ru.is>
2016-12-01 12:29:02 +0000

    Update legendService.h

--- legendService.h
+++ legendService.h
@@ -12,6 +12,7 @@ class LegendService
 public:
     LegendService();
     vector <Legend> getLegends ();
+    bool checkForValid(Legend legend);
     vector <Legend> getSort ();
     vector <Legend> getSortGender (); 
     vector <Legend> getSortDateOfBirth ();
@@ -19,7 +20,7 @@ public:
     vector <Legend> findLegend(string nameToFind);
     vector <Legend> findLegend(char genderToFind);
     vector <Legend> findLegend(int yearToFind, bool select);
-    void addLegend (string name, char gender, int born, int death);
+    void addLegend (string name, char gender, int born, int death, bool &boolValid);
     void deleteLegend(int index, vector<Legend> deleteVector);
 };
 
