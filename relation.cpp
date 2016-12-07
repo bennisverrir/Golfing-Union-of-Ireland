0714da3157a3720eaebd8be2a6bca906ccff4846
helgib16 <helgib16@ru.is>
2016-12-07 13:00:57 +0000

    Added the relation class

--- relation.cpp
+++ relation.cpp
@@ -1,6 +1,40 @@
 #include "relation.h"
 
+
+bool operator == (Relation &lhs, Relation &rhs)
+{
+    if(lhs._legendName == rhs._legendName && lhs._computerName == rhs._legendName)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 Relation::Relation()
 {
 
+}
+Relation::Relation (string legendName, string computerName)
+{
+    _legendName = legendName;
+    _computerName = computerName;
+}
+string Relation::getLegendName() const
+{
+    return _legendName;
+}
+string Relation::getComputerName()const
+{
+    return _computerName;
+}
+string Relation::setLegendName(string legendName)
+{
+    _legendName = legendName;
+}
+string Relation::setComputerName(string computerName)
+{
+    _computerName = computerName;
 }
--- relation.h
+++ relation.h
@@ -1,11 +1,24 @@
 #ifndef RELATION_H
 #define RELATION_H
 
+#include <string>
+using namespace std;
 
 class Relation
 {
 public:
     Relation();
+    Relation (string legendName, string computerName);
+
+    friend bool operator == (Relation &lhs, Relation &rhs);
+    string getLegendName() const;
+    string getComputerName()const;
+    string setLegendName(string legendName);
+    string setComputerName(string computerName);
+
+private:
+    string _legendName;
+    string _computerName;
 };
 
 #endif // RELATION_H
