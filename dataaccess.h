a3b3038a1cf9df14e4a412831ffc0c754cb33e18
Elvarorn <elvarornant@gmail.com>
2016-12-09 17:28:07 +0000

    Update dataaccess.h
    joke fall

--- dataaccess.h
+++ dataaccess.h
@@ -43,6 +43,7 @@ public:
     vector<Relation> findRelation(string nameToFind);
     void deleteRelation(Relation relationToDelete);
     void editRelation(Relation oldRelation, Relation editedRelation);
+    vector<string>joke();
 
 
 private:
