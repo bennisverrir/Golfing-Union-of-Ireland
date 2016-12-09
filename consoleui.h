a1cd2b32ba15b0edb4b8f5998831c6c6b3f5848c
kolliarnorsson <kolbeinna16@ru.is>
2016-12-09 23:11:30 +0000

    Update consoleui.h

--- consoleui.h
+++ consoleui.h
@@ -15,54 +15,45 @@ public:
     ConsoleUI();
     void run ();
 private:
-    Service _service;
-    //ComputerService  _cService;
-    void coutChoice(int command);
+
     void displayCommands();
+    void validateCommand (int &command);
+    void coutChoice(int command);
     void commandListScientists();
-    //void checkForFileError();
+    void ascDesc();
+    void commandListComputers();
+    void commandListRelations();
     void commandAddScientist();
-    void commandEditComputer();
     void getName(string &name);
     bool checkName(string name, bool flag);
     string rightName(string name);
     void getGender(char &gender);
+    void validateGender(char &gender);    
     void getBorn(int &born);
-    void validateCommand (int &command);
     void validateYear (int begin, int &input);
     void getDeath(int &death, int born);
     bool checkIfDead();
-    void commandDelete();
-    void commandFindScientist();
-    void subCommandFind(int command, vector <Legend> &toPrint);
-    void commandClear();
-    void fortyTwo();
-    void commandListComputers();
-    void commandAddComputer();
-    void getComputerType(int computerType);
-    bool getWasBuilt(bool wasBuilt);
-    void sortComputer(char sortComputerCommand, vector<Computer>& computers);
-    void commandListComputerTypes();
-    void addComputerType();
-    void commandFindComputer();
-    void commandEditScientist();
-    void ascDesc();
-    void validateGender(char &gender);
     void validateInputYN(char &toValidate);
-    void commandEditRelation();
-    void findRelation();
-    void commandListRelations();
-    void commandAddRelation();
-    void subCommandFindComputer(int command, vector <Computer> &toPrint);
+    void commandAddComputer();
     void getComputerName(string &computerName);
     void getBuildYear(int &buildYear, bool &wasBuilt);
     bool checkIfBuilt(bool &wasBuilt);
+    void commandListComputerTypes();
+    void addComputerType();   
+    void commandAddRelation();
+    void commandFindScientist();
+    void subCommandFind(int command, vector <Legend> &toPrint);
+    void commandFindComputer();
+    void subCommandFindComputer(int command, vector <Computer> &toPrint);
+    void findRelation();
+    void commandEditScientist();
+    void commandEditComputer();
+    void commandEditRelation();
     void deleteRelation();
+    void fortyTwo();  
     void outputJoke();
 
-
-
-
+    Service _service;
     int _numOfChoices;
     int _currentYear = 2016;
 };
