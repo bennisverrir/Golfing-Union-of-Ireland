22f70536c4ba367dade77a946819f5d0d2e71e8d
thorfinnurp <thorfinnur16@ru.is>
2016-12-09 18:03:50 +0000

    Update consoleui.cpp

--- consoleui.cpp
+++ consoleui.cpp
@@ -383,8 +383,8 @@ void ConsoleUI::coutChoice(int command)
     cout << "|"<< endl;
     cout << "|" << "Enter one of the following commands:" << '\t' <<'\t'<< '\t'<< '\t'<<" |" << endl;
     cout << "|" << "[1] - to " << com << " scientists" << '\t' <<'\t'<< '\t'<<'\t'<< '\t'<<" |" << endl;
-    cout << "|" << "[2] - to " << com << " computers" << setw(50) <<"   |" << endl;
-    cout << "|" << "[3] - to " << com << " relations" << setw(50) <<  "   |" << endl;
+    cout << "|" << "[2] - to "<<setw(1) << com << setw(1) << " computers" << setw(42) <<"   |" << endl;
+    cout << "|" << "[3] - to " << com << " relations" << setw(42) <<  "   |" << endl;
 
     cout << "|";
     for(int i = 0; i < 32; i++)
